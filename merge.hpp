7357d1c204eed2f601b2c9efbb2b2efc8d67df19
RiordanIX <majorjctaylor@gmail.com>
2014-10-16 13:56:54 -0400

    Changed how merge_sort is called.
    merge_sort is now just called with 2 parameters, because it makes much more sense.
    Unless there's a need to only sort a certain part of an array, which I don't see
    being very useful.

--- merge.hpp
+++ merge.hpp
@@ -4,7 +4,10 @@
 // forward statements of functions.
 
 template<class T>
-void merge_sort(T A[], T temp[], int begin, int end);
+void merge_sort(T A[], const int size);
+
+template<class T>
+void _split(T A[], T temp[], int begin, int end);
 
 template<class T>
 void _merge(T A[], T temp[], int begin, int mid, int end);
@@ -16,19 +19,28 @@ void _copy(T A[], T temp[], int begin, int end);
 
 // Implementation of functions.
 
+/// Takes an array and its size.  Creates a temporary array in order to do the
+/// sort.  
+template<class T>
+void merge_sort(T A[], const int size)
+{
+	T temp[size];
+	_split(A, temp, 0, size);
+	// Will automatically delete temp when loses scope.
+}
 
 template<class T> // temp is the empty array.
-void merge_sort(T A[], T temp[], int begin, int end)
+void _split(T A[], T temp[], int begin, int end)
 {
 	if(end - begin < 2) // if size of array is == 1 
 		return;
 	// recursively split into halves until size is 1,
 	// then merge and go back up.
 	int mid = (end + begin) / 2;
-	merge_sort(A, temp, begin, mid);	// mid will be excluded.
-	merge_sort(A, temp, mid, end);		// mid will be included.
-	_merge(A, temp, begin, mid, end);
-	_copy (A, temp, begin, end);
+	_split(A, temp, begin, mid);	// mid will be excluded.
+	_split(A, temp, mid, end);		// mid will be included.
+	_merge(A, temp, begin, mid, end);	// merge them back again.
+	_copy (A, temp, begin, end);		// Copy sorted array to original array.
 }
 
 template<class T> // temp is the empty array to have the sorted part applied.
