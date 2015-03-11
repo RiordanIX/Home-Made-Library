a1c08bb93d91e8f2e12d0090b723c72c10e3ff27
Major Taylor <majorjctaylor@gmail.com>
2015-03-11 09:53:37 -0400

    Added doubly LL, removed old LL
    Added the files, but not the implementations of Doubly linked lists.
    Will code later, this is mainly a place holder.

---
+++ DLlist.hpp
@@ -0,0 +1,10 @@
+#ifndef DLLIST_HPP
+#define DLLIST_HPP
+/*****************************************************************************
+ * Creates a templated Doubly linked list. This file is the 
+ *****************************************************************************/
+
+#include "DLlistIMP.hpp"
+
+
+#endif // DLLIST_HPP
